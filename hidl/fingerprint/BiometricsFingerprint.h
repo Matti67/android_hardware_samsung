7ba00f3cd411faf2dc156d33d69523f7021024fc
Tim Zimmermann <tim@linux4.de>
2021-11-29 20:51:07 +0100

    fingerprint: implement gestures
    [TenSeventy7: Re-implement as we are going to use event handling on this.]
    
    Change-Id: I66dfe09e569fcaea295649cce69350f4d4003eb4
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- hidl/fingerprint/BiometricsFingerprint.h
+++ hidl/fingerprint/BiometricsFingerprint.h
@@ -19,6 +19,10 @@
 #include <chrono>
 #include <thread>
 
+#ifdef HAS_FINGERPRINT_GESTURES
+#include <linux/uinput.h>
+#endif
+
 #include <hardware/fingerprint.h>
 #include <hardware/hardware.h>
 #include <hidl/MQDescriptor.h>
@@ -80,6 +84,7 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     int waitForSensor(std::chrono::milliseconds pollWait, std::chrono::milliseconds timeOut);
     static void notify(
         const fingerprint_msg_t* msg); /* Static callback for legacy HAL implementation */
+    void handleEvent(int eventCode);
     static Return<RequestStatus> ErrorFilter(int32_t error);
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);
     static FingerprintAcquiredInfo VendorAcquiredFilter(int32_t error, int32_t* vendorCode);
@@ -88,6 +93,9 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
 
     std::mutex mClientCallbackMutex;
     sp<IBiometricsFingerprintClientCallback> mClientCallback;
+#ifdef HAS_FINGERPRINT_GESTURES
+    int uinputFd;
+#endif
 
     int (*ss_fingerprint_close)();
     int (*ss_fingerprint_open)(const char* id);
